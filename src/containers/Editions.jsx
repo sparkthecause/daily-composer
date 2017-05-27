57460181ad3684ee41316ace4678f070d1a09ebb
Charles Burgess <consulting@cvburgess.com>
2017-05-27 08:44:17 -0400

    add new rows and header to dashboard

--- src/containers/Editions.jsx
+++ src/containers/Editions.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
 import moment from 'moment';
-import { Link } from 'react-router';
+import DashboardHeader from '../components/DashboardHeader';
+import DashboardRow from '../components/DashboardRow';
 
 class Editions extends React.Component {
 
   render() {
-    const { children, data: { editions, loading, error } } = this.props;
+    const { children, data: { editions, loading, error }, location: { query: { weekOf } } } = this.props;
 
     if (loading) {
       return <div>loading...</div>;
@@ -17,20 +18,31 @@ class Editions extends React.Component {
       return <div>Error!</div>;
     }
 
-    const editionLinks = editions.map((edition) => {
-      return (
-        <li key={edition.id}>
-          <Link to={`/editions/${edition.publishOn}`}>
-            {edition.publishOn}
-          </Link>
-        </li>
-      );
-    })
+    const startOfWeek = moment.utc(weekOf).startOf('isoWeek');
+    const endOfWeek = moment.utc(weekOf).endOf('isoWeek');
+    const headerTitle = `${startOfWeek.format('MMM D')} - ${endOfWeek.format('MMM D')}`;
+    const editionRows = editions.map((edition) =>
+      <DashboardRow
+        key={edition.id}
+        messagesBounced={1}
+        messagesOpened={2}
+        messagesSent={3}
+        publishOn={edition.publishOn}
+        subject={edition.subject} />
+    );
+    const Dashboard = (
+      <div>
+        <DashboardHeader
+          nextDate={endOfWeek.add(1, 'days').format('YYYY-MM-DD')}
+          previousDate={startOfWeek.subtract(1, 'days').format('YYYY-MM-DD')}
+          title={headerTitle} />
+        {editionRows}
+      </div>
+    );
 
     return (
       <div>
-        {children ? null : <ul>{editionLinks}</ul>}
-        {children}
+        {children ? children : Dashboard}
       </div>
     );
 
@@ -43,6 +55,7 @@ const EDITIONS_QUERY = gql`
     editions(publishOnOrAfter: $begOfWeek publishOnOrBefore: $endOfWeek) {
       id
       publishOn (format: "YYYY-MM-DD")
+      subject
     }
   }`;
 
