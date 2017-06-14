08dd96e78f0d0c3c64ac96b26e66162349e957bf
Charles Burgess <consulting@cvburgess.com>
2017-06-13 22:16:39 -0400

    add debounce to subject
    closes #43

--- src/components/Subject.jsx
+++ src/components/Subject.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import DebounceInput from 'react-debounce-input';
 
-const Subject = ({ onChange, subject }) => (
-  <input
+const Subject = ({ onChange, subject }) =>
+  <DebounceInput
     className="subject"
     placeholder="Subject"
     type="text"
     value={subject || ''}
+    debounceTimeout={500}
     onChange={(event) => onChange(event.target.value)} />
-);
 
 Subject.propTypes = {
   subject: React.PropTypes.string,
